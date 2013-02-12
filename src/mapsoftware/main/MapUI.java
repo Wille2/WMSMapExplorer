a896afb21abd2f2500c219985211a45c91caf6d5
Wille2 <villeahti@hotmail.com>
2013-02-12 13:00:42 +0200

    some refactoring

--- src/mapsoftware/main/MapUI.java
+++ src/mapsoftware/main/MapUI.java
@@ -38,218 +38,241 @@ import mapsoftware.wms.domain.ServiceCapabilitiesInformation;
  */
 public class MapUI extends JFrame {
 
-    private final WMServiceStrategy wmServiceStrategy;
-    private LocationInformation area;
-    private List<LayerInformation> selectedLayers;
+	private final WMServiceStrategy wmServiceStrategy;
+	private LocationInformation locationInfo;
+	private List<LayerInformation> selectedLayers;
 
-    // TODO: Move installation
-    private final JLabel lblMapImage = new JLabel();
-    private final JLayeredPane leftPanel = new JLayeredPane();
-    private JPanel pnlCheckBox;
+	private JLabel lblMapImage;
+	private JLayeredPane pnlLeftNavigation;
+	private JPanel pnlCheckBox;
 
-    private final JButton btnRefresh = new JButton("Refresh");
-    private final JButton btnLeft = new JButton("<");
-    private final JButton btnRight = new JButton(">");
-    private final JButton btnUp = new JButton("^");
-    private final JButton btnDown = new JButton("v");
-    private final JButton btnZoomIn = new JButton("+");
-    private final JButton btnZoomOut = new JButton("-");
+	private JButton btnRefresh;
+	private JButton btnLeft;
+	private JButton btnRight;
+	private JButton btnUp;
+	private JButton btnDown;
+	private JButton btnZoomIn;
+	private JButton btnZoomOut;
 
-    public static void main(String[] args) {
-        new MapUI(WMServiceFactory.getWMService(WMServiceProvider.WORLD_MAP));
-    }
+	public static void main(String[] args) {
+		new MapUI(WMServiceFactory.getWMService(WMServiceProvider.WORLD_MAP));
+	}
 
-    public MapUI(WMServiceStrategy wmServiceStrategy) {
-        super();
-        this.wmServiceStrategy = wmServiceStrategy;
-        init();
-    }
+	public MapUI(WMServiceStrategy wmServiceStrategy) {
+		super();
+		this.wmServiceStrategy = wmServiceStrategy;
+		init();
+	}
 
-    /**
-     * <p>
-     * Inits UI
-     * </p>
-     * 
-     */
-    private void init() {
-        // TODO: Edit layout: improve panel organization and whole appearance.
-        // Make MenuBar
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new BorderLayout());
+	/**
+	 * <p>
+	 * Inits UI
+	 * </p>
+	 * 
+	 */
+	private void init() {
+		// TODO: Edit layout: improve panel organization and whole appearance.
+		// Make MenuBar
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout());
 
-        ServiceCapabilitiesInformation serviceInfo = wmServiceStrategy
-                .getCapabilities();
-        List<LayerInformation> layers = serviceInfo.getLayerInformations();
-        area = serviceInfo.getLocationInformation();
+		ServiceCapabilitiesInformation serviceInfo = wmServiceStrategy
+				.getCapabilities();
+		List<LayerInformation> layers = serviceInfo.getLayerInformations();
+		locationInfo = serviceInfo.getLocationInformation();
 
-        attachButtonListeners();
+		initPanels();
+		initButtons();
+		attachButtonListeners();
 
-        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-        leftPanel.setMaximumSize(new Dimension(100, 600));
+		pnlLeftNavigation.setLayout(new BoxLayout(pnlLeftNavigation,
+				BoxLayout.Y_AXIS));
+		pnlLeftNavigation
+				.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		pnlLeftNavigation.setMaximumSize(new Dimension(100, 600));
 
-        pnlCheckBox = new JPanel();
-        pnlCheckBox.setLayout(new BoxLayout(pnlCheckBox, BoxLayout.Y_AXIS));
-        leftPanel.add(pnlCheckBox);
+		pnlCheckBox.setLayout(new BoxLayout(pnlCheckBox, BoxLayout.Y_AXIS));
+		pnlLeftNavigation.add(pnlCheckBox);
 
-        for (LayerInformation layerInfo : layers) {
-            pnlCheckBox.add(new LayerCheckBox(layerInfo, true));
-        }
+		for (LayerInformation layerInfo : layers) {
+			pnlCheckBox.add(new LayerCheckBox(layerInfo, true));
+		}
 
-        leftPanel.add(btnRefresh);
-        leftPanel.add(Box.createVerticalStrut(20));
-        leftPanel.add(btnLeft);
-        leftPanel.add(btnRight);
-        leftPanel.add(btnUp);
-        leftPanel.add(btnDown);
-        leftPanel.add(btnZoomIn);
-        leftPanel.add(btnZoomOut);
-        
-        checkSelectedLayers();
-        lblMapImage.setIcon(new ImageIcon(wmServiceStrategy.getMap(
-                selectedLayers, area)));
+		pnlLeftNavigation.add(btnRefresh);
+		pnlLeftNavigation.add(Box.createVerticalStrut(20));
+		pnlLeftNavigation.add(btnLeft);
+		pnlLeftNavigation.add(btnRight);
+		pnlLeftNavigation.add(btnUp);
+		pnlLeftNavigation.add(btnDown);
+		pnlLeftNavigation.add(btnZoomIn);
+		pnlLeftNavigation.add(btnZoomOut);
+	
+		initImage();
 
-        add(lblMapImage, BorderLayout.EAST);
-        add(leftPanel, BorderLayout.WEST);
+		add(lblMapImage, BorderLayout.EAST);
+		add(pnlLeftNavigation, BorderLayout.WEST);
 
-        pack();
-        setVisible(true);
-        setResizable(false);
-    }
+		pack();
+		setVisible(true);
+		setResizable(false);
+	}
 
-    private void attachButtonListeners() {
-        ButtonListener bl = new ButtonListener();
-        btnRefresh.addActionListener(bl);
-        btnLeft.addActionListener(bl);
-        btnRight.addActionListener(bl);
-        btnUp.addActionListener(bl);
-        btnDown.addActionListener(bl);
-        btnZoomIn.addActionListener(bl);
-        btnZoomOut.addActionListener(bl);
-    }
+	private void initImage() {
+		checkSelectedLayers();
+		lblMapImage.setIcon(new ImageIcon(wmServiceStrategy.getMap(
+				selectedLayers, locationInfo)));
+	}
 
-    /**
-     * <p>
-     * ButtonListener for all UI components
-     * </p>
-     * 
-     */
-    private class ButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == btnRefresh) {
-                updateMapImage();
-            }
-            if (e.getSource() == btnLeft) {
-                area.move(Operation.LEFT);
-                updateMapImage();
-            }
-            if (e.getSource() == btnRight) {
-                area.move(Operation.RIGHT);
-                updateMapImage();
-            }
-            if (e.getSource() == btnUp) {
-                area.move(Operation.UP);
-                updateMapImage();
-            }
-            if (e.getSource() == btnDown) {
-                area.move(Operation.DOWN);
-                updateMapImage();
-            }
-            if (e.getSource() == btnZoomIn) {
-                area.move(Operation.ZOOM_IN);
-                updateMapImage();
-            }
-            if (e.getSource() == btnZoomOut) {
-                area.move(Operation.ZOOM_OUT);
-                updateMapImage();
-            }
-        }
-    }
+	private void initButtons() {
+		btnRefresh = new JButton("Refresh");
+		btnLeft = new JButton("<");
+		btnRight = new JButton(">");
+		btnUp = new JButton("^");
+		btnDown = new JButton("v");
+		btnZoomIn = new JButton("+");
+		btnZoomOut = new JButton("-");
+	}
 
-    /**
-     * <p>
-     * CheckBox that keeps information about WMS layers
-     * </p>
-     * 
-     * @author Ville Ahti
-     * 
-     */
-    private class LayerCheckBox extends JCheckBox {
-        private final LayerInformation layerInformation;
+	private void initPanels() {
+		lblMapImage = new JLabel();
+		pnlCheckBox = new JPanel();
+		pnlLeftNavigation = new JLayeredPane();
+	}
 
-        public LayerCheckBox(LayerInformation layerInfo, boolean selected) {
-            super(layerInfo.getTitle(), null, selected);
-            this.layerInformation = layerInfo;
-        }
+	private void attachButtonListeners() {
+		ButtonListener bl = new ButtonListener();
+		btnRefresh.addActionListener(bl);
+		btnLeft.addActionListener(bl);
+		btnRight.addActionListener(bl);
+		btnUp.addActionListener(bl);
+		btnDown.addActionListener(bl);
+		btnZoomIn.addActionListener(bl);
+		btnZoomOut.addActionListener(bl);
+	}
 
-        public LayerInformation getLayerInformation() {
-            return layerInformation;
-        }
+	/**
+	 * <p>
+	 * ButtonListener for all UI components
+	 * </p>
+	 * 
+	 */
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnRefresh) {
+				updateMapImage();
+			}
+			if (e.getSource() == btnLeft) {
+				locationInfo.move(Operation.LEFT);
+				updateMapImage();
+			}
+			if (e.getSource() == btnRight) {
+				locationInfo.move(Operation.RIGHT);
+				updateMapImage();
+			}
+			if (e.getSource() == btnUp) {
+				locationInfo.move(Operation.UP);
+				updateMapImage();
+			}
+			if (e.getSource() == btnDown) {
+				locationInfo.move(Operation.DOWN);
+				updateMapImage();
+			}
+			if (e.getSource() == btnZoomIn) {
+				locationInfo.move(Operation.ZOOM_IN);
+				updateMapImage();
+			}
+			if (e.getSource() == btnZoomOut) {
+				locationInfo.move(Operation.ZOOM_OUT);
+				updateMapImage();
+			}
+		}
+	}
 
-    }
+	/**
+	 * <p>
+	 * CheckBox that keeps information about WMS layers
+	 * </p>
+	 * 
+	 * @author Ville Ahti
+	 * 
+	 */
+	private class LayerCheckBox extends JCheckBox {
+		private final LayerInformation layerInformation;
 
-    /**
-     * <p>
-     * Updates map image
-     * </p>
-     */
-    private void updateMapImage() {
-        new Thread() {
-            @Override
-            public void run() {
-                SwingUtilities.invokeLater(new MapUpdater());
-            }
-        }.start();
-    }
+		public LayerCheckBox(LayerInformation layerInfo, boolean selected) {
+			super(layerInfo.getTitle(), null, selected);
+			this.layerInformation = layerInfo;
+		}
 
-    /**
-     * @return request parameter from chosen LayerCheckbox components
-     */
-    // TODO: Check that at least one is checked to prevent no layers checked
-    // errors with WMS (DO EVERYTHING Differently)
-    private void checkSelectedLayers() {
-    	if(selectedLayers == null) {
-    		selectedLayers = new ArrayList<LayerInformation>();
-    	}
-    	selectedLayers.clear();
-      
-        for (Component comLayer : pnlCheckBox.getComponents()) {
-            if (((LayerCheckBox) comLayer).isSelected()) {
-                        selectedLayers.add(((LayerCheckBox) comLayer).getLayerInformation());
-            }
-        }
-    }
+		public LayerInformation getLayerInformation() {
+			return layerInformation;
+		}
 
-    /**
-     * <p>
-     * Runnable class which updates map
-     * </p>
-     * 
-     * @author Ville Ahti
-     */
-    private class MapUpdater implements Runnable {
-        /*
-         * (non-Javadoc)
-         * 
-         * @see java.lang.Runnable#run()
-         */
-        @Override
-        public void run() {
-            updateMap();
-        }
+	}
 
-        /**
-         * <p>
-         * updates map UIs image
-         * </p>
-         */
-        private void updateMap() {
-            checkSelectedLayers();
-            lblMapImage.setIcon(new ImageIcon(wmServiceStrategy.getMap(selectedLayers,
-                    area)));
-        }
+	/**
+	 * <p>
+	 * Updates map image
+	 * </p>
+	 */
+	private void updateMapImage() {
+		new Thread() {
+			@Override
+			public void run() {
+				SwingUtilities.invokeLater(new MapUpdater());
+			}
+		}.start();
+	}
 
-    }
+	/**
+	 * @return request parameter from chosen LayerCheckbox components
+	 */
+	// TODO: Check that at least one is checked to prevent no layers checked
+	// errors with WMS (DO EVERYTHING Differently)
+	private void checkSelectedLayers() {
+		if (selectedLayers == null) {
+			selectedLayers = new ArrayList<LayerInformation>();
+		}
+		selectedLayers.clear();
+
+		for (Component comLayer : pnlCheckBox.getComponents()) {
+			if (((LayerCheckBox) comLayer).isSelected()) {
+				selectedLayers.add(((LayerCheckBox) comLayer)
+						.getLayerInformation());
+			}
+		}
+	}
+
+	/**
+	 * <p>
+	 * Runnable class which updates map
+	 * </p>
+	 * 
+	 * @author Ville Ahti
+	 */
+	private class MapUpdater implements Runnable {
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Runnable#run()
+		 */
+		@Override
+		public void run() {
+			updateMap();
+		}
+
+		/**
+		 * <p>
+		 * updates map UIs image
+		 * </p>
+		 */
+		private void updateMap() {
+			checkSelectedLayers();
+			lblMapImage.setIcon(new ImageIcon(wmServiceStrategy.getMap(
+					selectedLayers, locationInfo)));
+		}
+
+	}
 
 }
